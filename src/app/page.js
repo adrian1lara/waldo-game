42e56e3aadc059f118a94876c2487942b987618e
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-01-31 00:51:46 -0500

    Added Component Timer in line 10

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import GameBoard from "./components/gameBoard";
 import ImageNav from "./components/imageNav";
+import Timer from "./components/timer";
 
 export default function Home() {
   return (
     <div>
       <ImageNav />
       <h1>Find Waldo</h1>
+      <Timer />
       <GameBoard />
     </div>
   );
