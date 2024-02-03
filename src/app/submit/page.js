3239566af1decbfa6c06f799934a0888e06e4453
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-02 20:12:32 -0500

    styling tags, and added useeffect

--- src/app/submit/page.js
+++ src/app/submit/page.js
@@ -1,10 +1,17 @@
 "use client"
+import { redirect, useRouter } from "next/navigation"
+import "../styles/global.css"
 import { useEffect, useState } from "react"
+
 
 export default function LeaderBoardForm() {
     const [error, setError] = useState(null)
     const [name, setName] = useState("")
+    const [time, setTime] = useState("")
 
+    useEffect(() => {
+        setTime(localStorage.getItem("timeRecord"))
+    })
 
     const submitToLeaderboard = async(e) => {
         e.preventDefault()
@@ -38,16 +45,26 @@ export default function LeaderBoardForm() {
 
 
     return (
-        <div>
-            <h1>You Won!</h1>
-            <h1>Leaderboard Form</h1>
-            <form >
-                <label>Insert Your Name</label>
-                <br />
-                <input placeholder="Your name" type="text" maxLength={10} min={3} value={name} onChange={(e) => setName(e.target.value)}/>
-                <button type="submit" onClick={(e) => submitToLeaderboard(e)}>Send</button>
-                {error && <p> {error}</p>}
-            </form>
+        <div className="h-screen flex items-center justify-center">
+            <div>
+                <div className="text-center">
+                <h1 className="text-3xl font-bold text-blue-500">You Won!</h1>
+                <p className="mt-2">Your time was  {time}</p>
+                </div>
+                
+                <form className="mt-4 shadow-lg p-5 rounded-xl">
+                    <label>Insert Your Nickname</label>
+                    <br />
+                    <input 
+                    className="mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username"
+                    placeholder="Annita" type="text" maxLength={10} min={3} value={name} onChange={(e) => setName(e.target.value)}/>
+                    {error && <p className="text-red-500 text-xs italic"> {error}</p>}
+                    <button 
+                    className="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit" 
+                    onClick={(e) => submitToLeaderboard(e)}>Send</button>
+                </form>
+            </div>
         </div>
     )
 }
