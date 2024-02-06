d8a49d4ee6d267bd2279ec31755a17b7bcd14ca6
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-05 21:37:57 -0500

    add func to convert string on time and sort leaderboard by time

--- src/app/components/leaderboard.js
+++ src/app/components/leaderboard.js
@@ -6,7 +6,6 @@ export default function LeaderBoard() {
 
     const [ leaderboard, setLeaderboard ] = useState(null)
 
-
     const handleLeaderboard = async () => {
         try {
             const res = await fetch("http://localhost:3000/api/leaderboard")
@@ -23,29 +22,45 @@ export default function LeaderBoard() {
             setLeaderboard([])
         }
     }
+
+    function convertTimeStringToSeconds(timeString) {
+        const [minutes, seconds] = timeString.split(":").map(Number);
+        return minutes * 60 + seconds;
+    }
    
     useEffect(()=> {
         handleLeaderboard()
-        console.log(leaderboard)
     }, [])
+
+
 
     return(
         <div>
-      <h2>LeaderBoard</h2>
-      {leaderboard ? (
-        <div>
-          {leaderboard.map((leader, index) => (
-            <div key={leader._id}>
-            <p className="text-3xl">{index + 1}</p>
-              <p>{leader.name}</p>
-              <p>{leader.timer}</p>
-              <p>{leader.clicks}</p>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>No leaderboard data available.</p>
-      )}
+            <h2>Leaderboard</h2>
+        <table className="min-w-full text-left text-sm font-light">
+            
+            <thead className="border-b font-medium dark:border-neutral-500">
+            <tr>
+                <th scope="col" className="px-6 py-4">Position</th>
+                <th scope="col" className="px-6 py-4">Name</th>
+                <th scope="col" className="px-6 py-4">Clicks</th>
+                <th scope="col" className="px-6 py-4">Timer</th>
+            </tr>
+            </thead>
+
+                <tbody >
+
+                    {leaderboard?.sort((a, b) => { return convertTimeStringToSeconds(a.timer)  - convertTimeStringToSeconds(b.timer) }).map((leader, index) => (
+                        <tr key={leader._id} className="border-b dark:border-neutral-500">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">{index + 1}</td>
+                        <td className="whitespace-nowrap px-6 py-4">{leader.name}</td>
+                        <td className="whitespace-nowrap px-6 py-4">{leader.clicks}</td>
+                        <td className="whitespace-nowrap px-6 py-4">{leader.timer}</td>
+                    </tr>
+                    ))}
+                </tbody>
+        </table>
+        
     </div>
   );
 }
