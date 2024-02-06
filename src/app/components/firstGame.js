b89ff8c67edfa126ac38b35fe8a3913278f583bb
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-05 23:18:32 -0500

    add bg-white to link and  text-black to h3

--- src/app/components/firstGame.js
+++ src/app/components/firstGame.js
@@ -6,8 +6,8 @@ export default function FirstLevel() {
     return (
         <div>
             <div className="grid justify-items-center max-w-fit">
-                <Link href={"/first-level"} className="rounded-xl shadow-lg p-2 transition duration-300 ease-in-out hover:shadow-xl hover:shadow-blue-500/50 hover:scale-125">
-                    <h3 className="text-2xl text-center">Find Waldo</h3>
+                <Link href={"/first-level"} className="rounded-xl shadow-lg p-2 transition duration-300 ease-in-out hover:shadow-xl hover:shadow-blue-500/50 hover:scale-125 bg-white">
+                    <h3 className="text-2xl text-center text-black">Find Waldo</h3>
                     <Image
                         src={waldo}
                         height={150}
