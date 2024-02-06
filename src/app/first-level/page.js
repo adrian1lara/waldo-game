71f2ba795291b0ae02930391bdcf406c443eedfd
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-05 21:47:31 -0500

    add tailwind styles

--- src/app/first-level/page.js
+++ src/app/first-level/page.js
@@ -1,12 +1,18 @@
 import ImageNav from "../components/imageNav"
 import GameBoard from "../components/gameBoard"
 import Timer from "../components/timer"
+import "../styles/global.css"
 
 export default function FirstGame() {
     return (
         <div>
+            <div className="flex  justify-center">
             <ImageNav />
+            <div className="w-1/2 flex justify-end">
             <Timer />
+            </div>
+            
+            </div>
             <GameBoard />
         </div>
     )
